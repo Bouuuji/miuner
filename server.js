3962c4aef4a69cafda0f8138850917eec1b6f572
Bouuuji <44290693+Bouuuji@users.noreply.github.com>
2018-10-19 12:14:20 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
-// server.js
-// where your node app starts
-
-// init project
+const http = require('http');
 const express = require('express');
 const app = express();
+app.get("/", (request, response) => {
+  console.log(Date.now() + " Ping Received");
+  response.sendStatus(200);
+});
+app.listen(process.env.PORT);
+setInterval(() => {
+  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+}, 280000);
 
 // we've started you off with Express, 
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
